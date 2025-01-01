af80a5d141d58b4d4e794aaee04efc8fbde195b7
Ashad Hosenbocus <ashad.hosenbocus@gmail.com>
2025-01-01 16:29:20 +0800

    Update model loading to use weights_only parameter for improved efficiency

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def get_model(model_name="se_resnext50_32x4d", num_classes=101):
 def load_model():
     model = get_model()
     path = "./pretrained.pth"  # Ensure this file is in the container
-    checkpoint = torch.load(path, map_location=device)
+    checkpoint = torch.load(path, map_location=device, weights_only=True)
     model.load_state_dict(checkpoint["state_dict"])
     model = model.to(device)
     model.eval()
